05c2b73057669ffd0d829006a238a79806f7d19e
Huy Tran <41718974+HuyTran72@users.noreply.github.com>
2022-09-29 18:21:49 -0700

    Update Code 2

--- ChavviCalcExampleApp.java
+++ ChavviCalcExampleApp.java
@@ -45,36 +45,75 @@ public class ChavviCalcExampleApp {
                 }
                 break;
             case "b":
-        System.out.printf("Enter a number: ");
-        number = input.nextLine();
-        //check if the number is validation
-        if (numberValidation(number)){
-        numberB = Float.parseFloat(number);
-        addValue(numberA, numberB);
-        }else{
-        System.out.println("Error: Unknow command");
-        addValue(numberA, numberB);
-        }
-break;
-case "+":
-numberA = numberA + numberB;
-addValue(numberA, numberB);
-break;
-case "-":
-numberA = numberA - numberB;
-addValue(numberA, numberB);
-break;
-case "*":
-numberA = numberA * numberB;
-addValue(numberA, numberB);
-break;
-case "/":
-if (numberB == 0){
-System.out.println("Error: Unable to divide by 0");
-addValue(numberA, numberB);
-}else{
-numberA = numberA / numberB;
-addValue(numberA, numberB);
+                System.out.printf("Enter a number: ");
+                number = input.nextLine();
+
+                //check if the number is validation
+                if (numberValidation(number)){
+                    numberB = Float.parseFloat(number);
+                    addValue(numberA, numberB);
+                }else{
+                    System.out.println("Error: Unknow command");
+                    addValue(numberA, numberB);
+                }
+                break;
+
+            case "+":
+                numberA = numberA + numberB;
+                addValue(numberA, numberB);
+                break;
+            case "-":
+                numberA = numberA - numberB;
+                addValue(numberA, numberB);
+                break;
+            case "*":
+                numberA = numberA * numberB;
+                addValue(numberA, numberB);
+                break;
+            case "/":
+                if (numberB == 0){
+                    System.out.println("Error: Unable to divide by 0");
+                    addValue(numberA, numberB);
+                }else{
+                    numberA = numberA / numberB;
+                    addValue(numberA, numberB);
+                }
+                break;
+
+            case "c":
+                addValue(0, 0);
+                break;
+            default:
+                System.out.println("ERROR: Unknown command");
+                addValue(numberA, numberB);
+                break;
+              }
+            }
+
+          }
+private static boolean numberValidation(String number) {
+    try {
+        Float.parseFloat(number);
+        return true;
+    } catch (NumberFormatException e) {
+        return false;
+    }
 }
-break;
-case "c":
+private static void addValue(float a, float b){
+    String value = String.format("A = %.3f B = %.3f", a, b);
+    System.out.println("----------------------------------------");
+    System.out.println("Chavvi Calc");
+    System.out.println("----------------------------------------");
+    System.out.println(value);
+    System.out.println("----------------------------------------");
+    System.out.println("a Enter a value for A");
+    System.out.println("b Enter a value for B");
+    System.out.println("+ Add");
+    System.out.println("- Subtract");
+    System.out.println("* Multiply");
+    System.out.println("/ Divide");
+    System.out.println("c Clear");
+    System.out.println("q Quit");
+    System.out.println("----------------------------------------");
+    System.out.printf("Enter a command: ");
+    }
